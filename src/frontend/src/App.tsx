e957f0e1af040801c6622aadf9be812564e8e9d3
Thomas Nielsen <thomas.nielsen98@gmail.com>
2024-06-17 13:24:45 +0900

    Fixed tab width

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState, createContext, useContext, useEffect } from "react";
 import {
-	Routes,
-	Route,
-	Link,
-	useNavigate,
-	useLocation,
-	Navigate,
-	Outlet,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useLocation,
+  Navigate,
+  Outlet,
 } from "react-router-dom";
 import axios from "axios";
 
@@ -17,231 +17,230 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 export default function App() {
-	return (
-		<AuthProvider>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route path="/" element={<PublicPage />} />
-					<Route path="/login" element={<LoginPage />} />
-					<Route
-						path="/protected"
-						element={
-							<RequireAuth>
-								<ProtectedPage />
-							</RequireAuth>
-						}
-					/>
-				</Route>
-			</Routes>
-		</AuthProvider>
-	);
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<PublicPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/protected"
+            element={
+              <RequireAuth>
+                <ProtectedPage />
+              </RequireAuth>
+            }
+          />
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
 }
 
 export function Header() {
-	let auth = useAuth();
-	let navigate = useNavigate();
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-						News
-					</Typography>
-					{!auth.user ? (
-						<Button
-							color="inherit"
-							onClick={() => {
-								navigate("/login");
-							}}
-						>
-							Login
-						</Button>
-					) : (
-						<>
-							<Box sx={{ pr: 1 }}>
-								<Typography variant="caption">
-									Logged in as {auth.user}{" "}
-								</Typography>
-							</Box>
-							<Button
-								color="inherit"
-								onClick={() => {
-									auth.signout(() => navigate("/"));
-								}}
-							>
-								Logout
-							</Button>
-						</>
-					)}
-				</Toolbar>
-			</AppBar>
-		</Box>
-	);
+  let auth = useAuth();
+  let navigate = useNavigate();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          {!auth.user ? (
+            <Button
+              color="inherit"
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Login
+            </Button>
+          ) : (
+            <>
+              <Box sx={{ pr: 1 }}>
+                <Typography variant="caption">
+                  Logged in as {auth.user}{" "}
+                </Typography>
+              </Box>
+              <Button
+                color="inherit"
+                onClick={() => {
+                  auth.signout(() => navigate("/"));
+                }}
+              >
+                Logout
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
 function Layout() {
-	return (
-		<div>
-			<Header />
-			<ul>
-				<li>
-					<Link to="/">Public Page</Link>
-				</li>
-				<li>
-					<Link to="/protected">Protected Page</Link>
-				</li>
-			</ul>
+  return (
+    <div>
+      <Header />
+      <ul>
+        <li>
+          <Link to="/">Public Page</Link>
+        </li>
+        <li>
+          <Link to="/protected">Protected Page</Link>
+        </li>
+      </ul>
 
-			<Outlet />
-		</div>
-	);
+      <Outlet />
+    </div>
+  );
 }
 
 interface AuthContextType {
-	user: any;
-	signin: (username: string, password: string, callback: VoidFunction) => void;
-	signout: (callback: VoidFunction) => void;
+  user: any;
+  signin: (username: string, password: string, callback: VoidFunction) => void;
+  signout: (callback: VoidFunction) => void;
 }
 
 let AuthContext = createContext<AuthContextType>(null!);
 
 function AuthProvider({ children }: { children: React.ReactNode }) {
-	const [user, setUser] = useState<any>(null);
-	const [loading, setLoading] = useState<Boolean>(true);
-	let navigate = useNavigate();
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState<Boolean>(true);
+  let navigate = useNavigate();
 
-	const config = {
-		withCredentials: true,
-		headers: {
-			"Content-Type": "application/json",
-		},
-	};
+  const config = {
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-	useEffect(() => {
-		axios
-			.get("http://localhost:5000/protected", config)
-			.then((resp) => {
-				setUser(resp.data.logged_in_as);
-			})
-			.catch((resp: any) => {
-				if (resp.response.status === 401) {
-					signout(() => navigate("/"));
-				}
-			})
-			.finally(() => {
-				setLoading(false);
-			});
-	}, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/protected", config)
+      .then((resp) => {
+        setUser(resp.data.logged_in_as);
+      })
+      .catch((resp: any) => {
+        if (resp.response.status === 401) {
+          signout(() => navigate("/"));
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-	let signin = (username: string, password: string, callback: VoidFunction) => {
-		const authData = { username: username, password: password };
-		axios
-			.post("http://localhost:5000/login", authData, config)
-			.then((resp: any) => {
-				axios
-					.get("http://localhost:5000/protected", config)
-					.then((resp: any) => {
-						setUser(resp.data.logged_in_as);
-					});
-			});
-	};
+  let signin = (username: string, password: string, callback: VoidFunction) => {
+    const authData = { username: username, password: password };
+    axios
+      .post("http://localhost:5000/login", authData, config)
+      .then((resp: any) => {
+        axios
+          .get("http://localhost:5000/protected", config)
+          .then((resp: any) => {
+            setUser(resp.data.logged_in_as);
+          });
+      });
+  };
 
-	let signout = (callback: VoidFunction) => {
-		axios
-			.post("http://localhost:5000/logout", null, config)
-			.then((resp: any) => {
-				setUser(null);
-				console.log("Logged out");
-			});
-	};
+  let signout = (callback: VoidFunction) => {
+    axios
+      .post("http://localhost:5000/logout", null, config)
+      .then((resp: any) => {
+        setUser(null);
+        console.log("Logged out");
+      });
+  };
 
-	let value = { user, signin, signout };
+  let value = { user, signin, signout };
 
-	return (
-		<>
-			{loading ? null : (
-				<AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-			)}
-		</>
-	);
+  return (
+    <>
+      {loading ? null : (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+      )}
+    </>
+  );
 }
 
 function useAuth() {
-	return useContext(AuthContext);
+  return useContext(AuthContext);
 }
 
 function RequireAuth({ children }: { children: JSX.Element }) {
-	let auth = useAuth();
-	let location = useLocation();
+  let auth = useAuth();
+  let location = useLocation();
 
-	if (!auth.user) {
-		// Redirect them to the /login page, but save the current location they were
-		// trying to go to when they were redirected. This allows us to send them
-		// along to that page after they login, which is a nicer user experience
-		// than dropping them off on the home page.
-		return <Navigate to="/login" state={{ from: location }} replace />;
-	}
+  if (!auth.user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
-	return children;
+  return children;
 }
 
 function LoginPage() {
-	let navigate = useNavigate();
-	let location = useLocation();
-	let auth = useAuth();
-	const [username, setUsername] = useState<string>("");
-	const [password, setPassword] = useState<string>("");
+  let navigate = useNavigate();
+  let location = useLocation();
+  let auth = useAuth();
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-	let from = location.state?.from?.pathname || "/";
+  let from = location.state?.from?.pathname || "/";
 
-	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
+    auth.signin(username, password, () => {
+      // Send them back to the page they tried to visit when they were
+      // redirected to the login page. Use { replace: true } so we don't create
+      // another entry in the history stack for the login page.  This means that
+      // when they get to the protected page and click the back button, they
+      // won't end up back on the login page, which is also really nice for the
+      // user experience.
+      navigate(from, { replace: true });
+    });
+  }
 
-		auth.signin(username, password, () => {
-			// Send them back to the page they tried to visit when they were
-			// redirected to the login page. Use { replace: true } so we don't create
-			// another entry in the history stack for the login page.  This means that
-			// when they get to the protected page and click the back button, they
-			// won't end up back on the login page, which is also really nice for the
-			// user experience.
-			navigate(from, { replace: true });
-		});
-	}
+  return (
+    <div>
+      <p>You must log in to view the page at {from}</p>
 
-	return (
-		<div>
-			<p>You must log in to view the page at {from}</p>
-
-			<form onSubmit={handleSubmit}>
-				<label>
-					Username:{" "}
-					<input
-						name="username"
-						type="text"
-						onChange={(e: any) => setUsername(e.target.value)}
-					/>
-				</label>{" "}
-				<br />
-				<label>
-					Password:{" "}
-					<input
-						name="password"
-						type="password"
-						onChange={(e: any) => setPassword(e.target.value)}
-					/>
-				</label>{" "}
-				<br />
-				<button type="submit">Login</button>
-			</form>
-		</div>
-	);
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            name="username"
+            type="text"
+            onChange={(e: any) => setUsername(e.target.value)}
+          />
+        </label>{" "}
+        <br />
+        <label>
+          Password:{" "}
+          <input
+            name="password"
+            type="password"
+            onChange={(e: any) => setPassword(e.target.value)}
+          />
+        </label>{" "}
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 function PublicPage() {
-	return <h3>Public</h3>;
+  return <h3>Public</h3>;
 }
 
 function ProtectedPage() {
-	return <h3>Protected</h3>;
+  return <h3>Protected</h3>;
 }
--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -6,15 +6,15 @@ import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<>
-				<CssBaseline />
-				<App />
-			</>
-		</BrowserRouter>
-	</React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <>
+        <CssBaseline />
+        <App />
+      </>
+    </BrowserRouter>
+  </React.StrictMode>
 );
